a9dca4f1e18ef03e35501a0a7288ab4069abbea2
adan_due <ing.adanduenas@gmail.com>
2024-08-12 13:26:06 -0600

    Contexto

--- states-machine-react/src/Machines/bookingMachine.jsx
+++ states-machine-react/src/Machines/bookingMachine.jsx
@@ -1,32 +1,66 @@
-import { createMachine } from "xstate";
+import { assign, createMachine } from "xstate";
 
 const bookingMachine = createMachine({
     id: "buy plane tickets",
     initial: "initial",
+    context: {
+        passengers: [],
+        selectedCountry: '',
+    },
     states: {
-    initial: {
-        on: {
-        START: "search",
+        initial: {
+            entry: assign(
+                ({context}) => {
+                    context.passengers = [],
+                    context.selectedCountry = ''
+                }
+            ),
+            on: {
+            START: {
+                target:'search',
+            },
+            },
+        },
+        search: {
+            on: {
+            CONTINUE: {
+                target: 'passengers',
+                actions: assign({
+                    selectedCountry: ({ event }) => event.selectedCountry,
+                })
+            },
+            CANCEL: "initial",
+            },
+        },
+        tickets: {
+            on: {
+            FINISH: "initial",
+            },
+        },
+        passengers: {
+            on: {
+            DONE: "tickets",
+            CANCEL: "initial",
+            ADD: {
+                target: 'passengers',
+                actions: assign(
+                    ({context, event}) => context.passengers.push(event.newPassenger)
+                )
+            },
+            },
         },
     },
-    search: {
-        on: {
-        CONTINUE: "passengers",
-        CANCEL: "initial",
+    
+    },
+    {
+        actions: {
+            imprimirInicio: () => console.log('Imprimir Inicio'),
+            imprimirEntrada: () => console.log('Imprimir Entrada a search'),
+            imprimirSalida: () => console.log('Imprimir Salida del search')
         },
-    },
-    tickets: {
-        on: {
-        FINISH: "initial",
-        },
-    },
-    passengers: {
-        on: {
-        DONE: "tickets",
-        CANCEL: "initial",
-        },
-    },
-    },
-});
+    }
+);
+
+
 
 export { bookingMachine }
